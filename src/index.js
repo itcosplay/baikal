b8067cd22305e226339e86be5d6af5aa80ca33c5
konstPialov <azure_dream@mail.ru>
2019-05-12 21:59:51 +0300

    need mobile menu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import './sass/fonts.sass';
 import './sass/reset.sass';
+import './sass/fonts.sass';
 // import './scss/slick.scss'
-import './sass/main.sass';
-
-
+import './sass/main-section.sass';
 // import './css/humburger.css';
-
 // import 'slick-carousel';
 
 
-console.log('hello world!');
+
+window.onload = () => {
+    var imgDefer = document.getElementsByTagName('img');
+    var bgDefer = document.getElementsByClassName('set-background');
+
+    for (var i=0; i<imgDefer.length; i++) {
+        if(imgDefer[i].getAttribute('data-src')) {
+            imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+        } 
+    }
+
+    for (var i=0; i<bgDefer.length; i++) {
+        if(bgDefer[i].getAttribute('data-bg')) {
+            bgDefer[i].setAttribute('style', bgDefer[i].getAttribute('data-bg'));
+        } 
+    } 
+};
