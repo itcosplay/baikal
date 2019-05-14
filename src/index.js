6978fb6db371cf0bbd4c668df7d86c960291fecf
konstPialov <azure_dream@mail.ru>
2019-05-14 13:30:20 +0300

    need make main content

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ import './sass/main-section.sass';
 
 
 window.onload = () => {
+    //setting property 'height' of .wrap-main-content
+    const mainContentWrap = document.querySelector('.wrap-main-content');
+    let clientHeight = document.documentElement.clientHeight;
+    let wrapLogoMenu = document.querySelector('.wrap-logo-menu');
+    let wrapLogoMenuHeight = window.getComputedStyle(wrapLogoMenu).height;
+    wrapLogoMenuHeight = Number(wrapLogoMenuHeight.substring(0, wrapLogoMenuHeight.length - 2));
+    
+    mainContentWrap.style.height = `${clientHeight - wrapLogoMenuHeight - 40}px`;
+    ////
+
+    //load images and background images after DOM
     var imgDefer = document.getElementsByTagName('img');
     var bgDefer = document.getElementsByClassName('set-background');
 
@@ -21,15 +32,32 @@ window.onload = () => {
         if(bgDefer[i].getAttribute('data-bg')) {
             bgDefer[i].setAttribute('style', bgDefer[i].getAttribute('data-bg'));
         } 
-    } 
+    }
+    ////
 };
 
 function main() {
     const mobileMenu = document.getElementById('mobile-menu');
 
     mobileMenu.onclick = () => {
-        console.log('click mob menu');
+        const mobMenu = document.querySelector('.bst-menu__mobile');
+        const mainContent = document.querySelector('.main-content');
+        
+        mobMenu.classList.toggle('hide-item');
+        mainContent.classList.toggle('hide-item');
     }
+
+    
+    window.onresize = () => {
+        const mainContentWrap = document.querySelector('.wrap-main-content');
+        let clientHeight = document.documentElement.clientHeight;
+        let wrapLogoMenu = document.querySelector('.wrap-logo-menu');
+        let wrapLogoMenuHeight = window.getComputedStyle(wrapLogoMenu).height;
+        wrapLogoMenuHeight = Number(wrapLogoMenuHeight.substring(0, wrapLogoMenuHeight.length - 2));
+        
+        mainContentWrap.style.height = `${clientHeight - wrapLogoMenuHeight - 40}px`;
+    }
+
 }
 
 main();
