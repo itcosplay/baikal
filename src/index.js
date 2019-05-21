1492a28b79175e3ab5298ee473566cb0347fb1da
konstPialov <azure_dream@mail.ru>
2019-05-21 17:39:10 +0300

    second page

--- src/index.js
+++ src/index.js
@@ -5,40 +5,43 @@ import './sass/fonts.sass';
 import './css/humburger.css';
 import './sass/modals.sass';
 import './sass/main-section.sass';
-import './sass/specificity.sass'
-import './sass/specificity_mobile.sass'
-import './sass/travel-format.sass'
-import './sass/with-us.sass'
-import './sass/about-us.sass'
-import './sass/footer.sass'
+import './sass/specificity.sass';
+import './sass/specificity_mobile.sass';
+import './sass/travel-format.sass';
+import './sass/with-us.sass';
+import './sass/about-us.sass';
+import './sass/footer.sass';
+import './sass/pages-menu.sass';
+import './sass/tours.sass';
 
 // import 'slick-carousel';
 
 
 window.onload = () => {
-    //setting property 'height' of .wrap-main-content
+    // setting property 'height' of .wrap-main-content
     const mainContentWrap = document.querySelector('.wrap-main-content');
-    if (mainContentWrap === null) return
-    
-    let clientHeight = document.documentElement.clientHeight;
-    let wrapLogoMenu = document.querySelector('.wrap-logo-menu');
-    let wrapLogoMenuHeight = window.getComputedStyle(wrapLogoMenu).height;
-    wrapLogoMenuHeight = Number(wrapLogoMenuHeight.substring(0, wrapLogoMenuHeight.length - 2));
-    
-    mainContentWrap.style.height = `${clientHeight - wrapLogoMenuHeight - 40}px`;
+
+    if (mainContentWrap !== null) {
+        let clientHeight = document.documentElement.clientHeight;
+        let wrapLogoMenu = document.querySelector('.wrap-logo-menu');
+        let wrapLogoMenuHeight = window.getComputedStyle(wrapLogoMenu).height;
+
+        wrapLogoMenuHeight = Number(wrapLogoMenuHeight.substring(0, wrapLogoMenuHeight.length - 2));
+        mainContentWrap.style.height = `${clientHeight - wrapLogoMenuHeight - 40}px`;
+    }
     ////
 
     //load images and background images after DOM
     var imgDefer = document.getElementsByTagName('img');
     var bgDefer = document.getElementsByClassName('set-background');
 
-    for (var i=0; i<imgDefer.length; i++) {
+    for (var i = 0; i < imgDefer.length; i++) {
         if(imgDefer[i].getAttribute('data-src')) {
             imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
         } 
     }
 
-    for (var i=0; i<bgDefer.length; i++) {
+    for (var i = 0; i<bgDefer.length; i++) {
         if(bgDefer[i].getAttribute('data-bg')) {
             bgDefer[i].setAttribute('style', bgDefer[i].getAttribute('data-bg'));
         } 
@@ -49,23 +52,39 @@ window.onload = () => {
 function main() {
     const mobileMenu = document.getElementById('mobile-menu');
 
-    mobileMenu.onclick = () => {
-        const mobMenu = document.querySelector('.bst-menu__mobile');
-        const mainContent = document.querySelector('.main-content');
+    if (mobileMenu !== null) {
+        mobileMenu.onclick = () => {
+            const mobMenu = document.querySelector('.bst-menu__mobile');
+            const mainContent = document.querySelector('.main-content');
         
-        mobMenu.classList.toggle('hide-item');
-        mainContent.classList.toggle('hide-item');
+            mobMenu.classList.toggle('hide-item');
+            mainContent.classList.toggle('hide-item');
+        }
     }
 
-    
+    const pagesHumburger = document.getElementById('pages-humburger');
+
+    if (pagesHumburger !== null) {
+        pagesHumburger.onclick = () => {
+            const mobMenu = document.querySelector('.pages-menu__mobile-menu');
+            // const mainContent = document.querySelector('.main-content');
+        
+            mobMenu.classList.toggle('hide-item');
+            // mainContent.classList.toggle('hide-item');
+        }
+    }
+
     window.onresize = () => {
         const mainContentWrap = document.querySelector('.wrap-main-content');
-        let clientHeight = document.documentElement.clientHeight;
-        let wrapLogoMenu = document.querySelector('.wrap-logo-menu');
-        let wrapLogoMenuHeight = window.getComputedStyle(wrapLogoMenu).height;
-        wrapLogoMenuHeight = Number(wrapLogoMenuHeight.substring(0, wrapLogoMenuHeight.length - 2));
-        
-        mainContentWrap.style.height = `${clientHeight - wrapLogoMenuHeight - 40}px`;
+
+        if (mainContentWrap !== null) {
+            let clientHeight = document.documentElement.clientHeight;
+            let wrapLogoMenu = document.querySelector('.wrap-logo-menu');
+            let wrapLogoMenuHeight = window.getComputedStyle(wrapLogoMenu).height;
+            wrapLogoMenuHeight = Number(wrapLogoMenuHeight.substring(0, wrapLogoMenuHeight.length - 2));
+            
+            mainContentWrap.style.height = `${clientHeight - wrapLogoMenuHeight - 40}px`;
+        }        
     }
 
     const mainContentBtn = document.querySelector('.main-content__button');
@@ -75,27 +94,28 @@ function main() {
     const overlay = document.querySelector('.overlay');
     const modalForm = document.querySelector('.modal-form');
 
-    mainContentBtn.onclick = () => {
+    if (mainContentBtn !== null) {
+        mainContentBtn.onclick = () => {
         
-        for (const section of sections) {
-            section.classList.add('have-blur');
+            for (const section of sections) {
+                section.classList.add('have-blur');
+            }
+    
+            overlay.classList.add('show-block');
+            modalForm.classList.add('show-block');
         }
-
-        overlay.classList.add('show-block');
-        modalForm.classList.add('show-block');
     }
 
-    modalCloseBtn.onclick = () => {
-        for (const section of sections) {
-            section.classList.remove('have-blur');
+    if (modalCloseBtn !== null) {
+        modalCloseBtn.onclick = () => {
+            for (const section of sections) {
+                section.classList.remove('have-blur');
+            }
+    
+            overlay.classList.remove('show-block');
+            modalForm.classList.remove('show-block');
         }
-
-        overlay.classList.remove('show-block');
-        modalForm.classList.remove('show-block');
     }
-
-    alert(file.name);
-
 }
 
 main();
