6993be118d622fec9f0d508590af7ac0d1964fb2
konstPialov <azure_dream@mail.ru>
2019-07-05 13:06:05 +0300

    start page description tour

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: [
-        './src/index.js'
+        './src/description_tour.js'
     ],
 
     output: {
@@ -74,75 +74,17 @@ module.exports = {
 
     plugins: [
         new MiniCssExtractPlugin({
-            filename: './style.bundle.css'
-            // filename: './style.bundle.css'
+            filename: './style.description_tour.css'
         }),
         new HtmlWebpackPlugin({
             hash: false,
-            template: './src/index.html',
-            filename: './index.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/ekspedecii.html',
-            filename: './ekspedecii.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/eko-tours.html',
-            filename: './eko-tours.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/location.html',
-            filename: './location.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/events.html',
-            filename: './events.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/contacts.html',
-            filename: './contacts.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/oleniy-fest.html',
-            filename: './oleniy-fest.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/ohotniki-za-mechtoj.html',
-            filename: './ohotniki-za-mechtoj.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/medvezhi-zori.html',
-            filename: './medvezhi-zori.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/sibirskie-orhidei.html',
-            filename: './sibirskie-orhidei.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/ornitologicheskie-nablyudeniya.html',
-            filename: './ornitologicheskie-nablyudeniya.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: './src/index_2.html',
-            filename: './index_2.html'
+            template: './src/description_tour.html',
+            filename: './description_tour.html'
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
             { from: './src/img', to: './img' },
-            { from: './src/favicon', to: './favicon' },
-            { from: './src/mailModal.php', to: './mailModal.php' },
-            { from: './src/mail.php', to: './mail.php' },
+            { from: './src/favicon', to: './favicon' }
         ]),
         new webpack.ProvidePlugin({
             $: 'jquery',
